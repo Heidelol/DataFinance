d1c2a5d1b8aaaa4338cc172e7b9bce6e510334a4
Heidelol <heideloliver@gmail.com>
2022-07-05 20:40:21 -0500

    add newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 function Newsletter() {
   return (
     <div className='w-full py-16 text-white'>
-        <div className='max-w-[1240px] mx-auto grid'>
+        <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
+            <div className='lg:col-span-2'>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold'>Want tips & tricks to optimize your flow?</h1>
+                <p>Sign up to our newsletter and stay up to date.</p>
+            </div>
+            <div>
 
+            </div>
         </div>
 
     </div>
